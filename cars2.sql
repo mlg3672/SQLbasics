0e0fc1e8b7a354b017d28b81ddee081ef3f5aae9
mlg3672 <michele.goe@gmail.com>
2015-07-27 12:51:41 +0800

    first commit

---
+++ cars2.sql
@@ -0,0 +1,16 @@
+insert into person (id, first_name, last_name, age) 
+ values (1, “Michele”, “Goe”, 33);
+
+insert into person (id, first_name, last_name, age) 
+ values (2, “Action”, “Jackson”, 44);
+
+insert into person (id, first_name, last_name, age) 
+ values (3, “Karate”, “Kid”, 21);
+
+insert into car(id, make, model, year, mpg) 
+ values (1, “Dodge”, “Ram”, 2000, 23.5);
+
+insert into car(id, make, model, year, mpg) 
+ values (2, “Toyota”, “Venza”, 2010, 26.5);
+
+insert into person_car (3, “Chevrolet”, ‘Grand-Prix’, 2011, 25.2);
